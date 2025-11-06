218586771308585a1bb4192e3678b53aac386082
rudrasingh993 <rudrasingh14513@gmail.com>
2025-11-06 22:34:29 +0530

    hh

--- api/get-api-key.js
+++ api/get-api-key.js
@@ -1,8 +1,8 @@
-module.exports = (req, res) => {
+export default function handler(req, res) {
   const apiKey = process.env.GEMINI_API_KEY;
   if (apiKey) {
     res.status(200).json({ apiKey });
   } else {
     res.status(500).json({ error: 'API key not found' });
   }
-};
+}
